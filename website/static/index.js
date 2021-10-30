2c45409eb71adce98bcc9370ab68f37273466cab
Toby_ <tobybarrett1993@gmail.com>
2021-10-30 03:17:29 +0100

    3

---
+++ website/static/index.js
@@ -0,0 +1,7 @@
+$("#addtable").click(function() {
+var index = 0; $('#tableCollection').clone().attr({'id': 'tableCollection' + index, hidden: false}).appendTo('#wrapper'); index++;
+});
+$("#addfield").click(function() {
+var index = 0; $('#tableCollection').clone().attr({'id': 'tableCollection' + index, hidden: false}).appendTo('#wrapper'); index++;
+});
+
