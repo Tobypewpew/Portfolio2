f6986c53f410a9c344c2cb7e9be282e3f6a86965
Toby_ <tobybarrett1993@gmail.com>
2021-10-31 00:58:10 +0100

    3

--- website/static/index.js
+++ website/static/index.js
@@ -1,7 +1,15 @@
-$("#addtable").click(function() {
-var index = 0; $('#tableCollection').clone().attr({'id': 'tableCollection' + index, hidden: false}).appendTo('#wrapper'); index++;
+$(function()
+{
+    $("#addtable").click(function () {
+        var index = 0;
+        $('#tableCollection').clone().attr({'id': 'tableCollection' + index, hidden: false}).appendTo('#wrapper');
+        index++;
+    });
+    $("#addfield").click(function () {
+        var index = 0;
+        $('#field').clone().attr({'id': 'field' + index}).appendTo('#fields');
+        index++;
+    });
 });
-$("#addfield").click(function() {
-var index = 0; $('#tableCollection').clone().attr({'id': 'tableCollection' + index, hidden: false}).appendTo('#wrapper'); index++;
-});
+
 
