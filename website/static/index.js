a2950949a5df2a501727936e3f1a6141e38aca9e
Toby_ <tobybarrett1993@gmail.com>
2021-11-02 22:42:33 +0000

    Added n value column reducing sizes of columns, add min-max value templates for dropdown list

--- website/static/index.js
+++ website/static/index.js
@@ -20,51 +20,52 @@ $(function()
     div.setAttribute('style','padding-top: 30px');
     div.innerHTML = '    <div class="container" >\n' +
         '      <div class="row" style="padding: 2px">\n' +
-        '        <div class="col-3"  align="right" style="line-height: 38px" >Input Table Name:</div>\n' +
-        '        <div class="col-3" >\n' +
+        '        <div class="col-2"  align="right" style="line-height: 38px" >Input Table Name:</div>\n' +
+        '        <div class="col-2" >\n' +
         '          <input type="text" class="form-control" name="Table" placeholder="Table Name"/>\n' +
         '        </div>\n' +
         '      </div>\n' +
         '\n' +
         '      <div class="row" style="padding: 2px">\n' +
-        '        <div class="col-3" align="right" style="line-height: 38px"  >Records Expected:</div>\n' +
-        '        <div class="col-3" >\n' +
+        '        <div class="col-2" align="right" style="line-height: 38px"  >Records Expected:</div>\n' +
+        '        <div class="col-2" >\n' +
         '          <input type="text" class="form-control" id="Records" name="Records" placeholder="Records"/>\n' +
         '        </div>\n' +
         '      </div>\n' +
         '      <div class="row" style="padding: 2px">\n' +
-        '        <div class="col-3" style="line-height: 38px"  >Field Name</div>\n' +
-        '        <div class="col-3" style="line-height: 38px"  >Datatype</div>\n' +
-        '        <div class="col-3" style="line-height: 38px"  >Minimum Bytes</div>\n' +
-        '        <div class="col-3" style="line-height: 38px"  >Maximum Bytes</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  >Field Name</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  >Datatype</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  >n-value</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  >Minimum Bytes</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  >Maximum Bytes</div>\n' +
         '      </div>\n' +
         '      <div id="fields">\n' +
         '        <div class="row" id = "field" style="padding: 2px">\n' +
-        '          <div class="col-3" >\n' +
+        '          <div class="col-2" >\n' +
         '            <input type="text" class="form-control" id="Field Name" name="Field Name" placeholder="Field Name"/>\n' +
         '          </div>\n' +
-        '          <div class="col-3" >\n' +
+        '          <div class="col-2" >\n' +
         '            <div class="dropdown">\n' +
         '              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"  data-toggle="dropdown" aria-expanded="false">\n' +
         '                Dropdown button\n' +
         '              </button>\n' +
         '              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">\n' +
-        '                <li><a class="dropdown-item" href="#">char(n)</a></li>\n' +
-        '                <li><a class="dropdown-item" href="#">varchar(n)</a></li>\n' +
-        '                <li><a class="dropdown-item" href="#">varchar(max)</a></li>\n' +
+        '                <li><a class="dropdown-item" value={"min-max":["0","1"]}>char(n)</a></li>\n' +
+        '                <li><a class="dropdown-item" value={"min-max":["0","1"]}>varchar(n)</a></li>\n' +
+        '                <li><a class="dropdown-item" value={"min-max":["0","1"]}>varchar(max)</a></li>\n' +
         '              </ul>\n' +
         '            </div>\n' +
         '          </div>\n' +
-        '          <div class="col-3" >100</div>\n' +
-        '          <div class="col-3" >1000</div>\n' +
+        '          <div class="col-2" >100</div>\n' +
+        '          <div class="col-2" >1000</div>\n' +
         '        </div>\n' +
 
         '      </div>\n' +
         '      <div class="row" style="padding: 2px">\n' +
-        '        <div class="col-3" style="line-height: 38px"  ></div>\n' +
-        '        <div class="col-3" style="line-height: 38px" align="right"  >Total Bytes:</div>\n' +
-        '        <div class="col-3" style="line-height: 38px"  >100</div>\n' +
-        '        <div class="col-3" style="line-height: 38px"  >1000</div>\n' +
+        '        <div class="col-2" style="line-height: 38px"  ></div>\n' +
+        '        <div class="col-2" style="line-height: 38px" align="right"  >Total Bytes:</div>\n' +
+        '        <div class="col-2" style="line-height: 38px" value="0" ></div>\n' +
+        '        <div class="col-2" style="line-height: 38px" value="0" ></div>\n' +
         '      </div>\n' +
 
         '\n' +
