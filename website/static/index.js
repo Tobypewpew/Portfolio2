2c618fa8bcee44e47de7b2d8da7913e27fd31356
Toby_ <tobybarrett1993@gmail.com>
2021-11-08 02:39:30 +0000

    updates variable placeholders and remove value

--- website/static/index.js
+++ website/static/index.js
@@ -12,45 +12,47 @@ $(document).on('click', '.addfield', function () {
         '                Datatype\n' +
         '              </button>\n' +
         '              <ul class="dropdown-menu col-xs-12" aria-labelledby="dropdownMenuButton">\n' +
-        '                <li><a class="dropdown-item" >Char          </a></li>\n' +
-        '                <li><a class="dropdown-item" >Varchar       </a></li>\n' +
-        '                <li><a class="dropdown-item" >Text          </a></li>\n' +
-        '                <li><a class="dropdown-item" >Nchar         </a></li>\n' +
-        '                <li><a class="dropdown-item" >NvarChar      </a></li>\n' +
-        '                <li><a class="dropdown-item" >Bit           </a></li>\n' +
-        '                <li><a class="dropdown-item" >Tinybit       </a></li>\n' +
-        '                <li><a class="dropdown-item" >Smallint      </a></li>\n' +
-        '                <li><a class="dropdown-item" >Int           </a></li>\n' +
-        '                <li><a class="dropdown-item" >Bigint        </a></li>\n' +
-        '                <li><a class="dropdown-item" >Decimal       </a></li>\n' +
-        '                <li><a class="dropdown-item" >Numeric       </a></li>\n' +
-        '                <li><a class="dropdown-item" >Smallmoney    </a></li>\n' +
-        '                <li><a class="dropdown-item" >Money         </a></li>\n' +
-        '                <li><a class="dropdown-item" >Real          </a></li>\n' +
-        '                <li><a class="dropdown-item" >Float         </a></li>\n' +
-        '                <li><a class="dropdown-item" >Date          </a></li>\n' +
-        '                <li><a class="dropdown-item" >Datetime      </a></li>\n' +
-        '                <li><a class="dropdown-item" >Datetime2     </a></li>\n' +
-        '                <li><a class="dropdown-item" >Datetimeoffset</a></li>\n' +
-        '                <li><a class="dropdown-item" >Smalldatetime </a></li>\n' +
-        '                <li><a class="dropdown-item" >Time          </a></li>\n' +
-        '                <li><a class="dropdown-item" >Binary        </a></li>\n' +
-        '                <li><a class="dropdown-item" >Varbinary     </a></li>\n' +
+        '                <li><a class="dropdown-item" value=1  >Char          </a></li>\n' +
+        '                <li><a class="dropdown-item" value=2  >Varchar       </a></li>\n' +
+        '                <li><a class="dropdown-item" value=3  >Nchar         </a></li>\n' +
+        '                <li><a class="dropdown-item" value=4  >NvarChar      </a></li>\n' +
+        '                <li><a class="dropdown-item" value=5  >Bit           </a></li>\n' +
+        '                <li><a class="dropdown-item" value=6  >Tinybit       </a></li>\n' +
+        '                <li><a class="dropdown-item" value=7  >Smallint      </a></li>\n' +
+        '                <li><a class="dropdown-item" value=8  >Int           </a></li>\n' +
+        '                <li><a class="dropdown-item" value=9 >Bigint        </a></li>\n' +
+        '                <li><a class="dropdown-item" value=10 >Decimal       </a></li>\n' +
+        '                <li><a class="dropdown-item" value=11 >Numeric       </a></li>\n' +
+        '                <li><a class="dropdown-item" value=12 >Smallmoney    </a></li>\n' +
+        '                <li><a class="dropdown-item" value=13 >Money         </a></li>\n' +
+        '                <li><a class="dropdown-item" value=14 >Real          </a></li>\n' +
+        '                <li><a class="dropdown-item" value=15 >Float         </a></li>\n' +
+        '                <li><a class="dropdown-item" value=16 >Date          </a></li>\n' +
+        '                <li><a class="dropdown-item" value=17 >Datetime      </a></li>\n' +
+        '                <li><a class="dropdown-item" value=18 >Datetime2     </a></li>\n' +
+        '                <li><a class="dropdown-item" value=19 >Datetimeoffset</a></li>\n' +
+        '                <li><a class="dropdown-item" value=20 >Smalldatetime </a></li>\n' +
+        '                <li><a class="dropdown-item" value=21 >Time          </a></li>\n' +
+        '                <li><a class="dropdown-item" value=22 >Binary        </a></li>\n' +
+        '                <li><a class="dropdown-item" value=23 >Varbinary     </a></li>\n' +
         '              </ul>\n' +
         '            </div>\n' +
         '          </div>\n' +
         '          <div class="col-2" >\n' +
-        '            <input type="text" class="form-control variable" name="variables" placeholder="n/a"/>\n' +
+        '            <input type="number" class="form-control variable" name="variables" placeholder="n/a" value="">\n' +
         '          </div>\n' +
         '          <div class="col-2" >100</div>\n' +
         '          <div class="col-2" >1000</div>\n';
     document.getElementById('fields'+T).appendChild(div);
-
 });
 
 $(document).on('click', '.dropdown-menu li a', function () {
     $(this).parents(".dropdown").find('.btn').html($(this).text());
-    $(this).parents(".row").find('.variable').val('hi')
+    var sel = $(this).attr("value");
+    var variable = ['n/a','n','n','n','n','n/a','n/a','n/a','n/a','n/a','p','p','n/a','n/a','n/a','n','n/a','n/a','p','p','n/a','n/a','n','n'];
+    $(this).parents(".row").find('.variable').attr('placeholder',variable[sel]);
+    $(this).parents(".row").find('.variable').val('');
+
 
 });
 
