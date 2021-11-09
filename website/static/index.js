54d7e0264e177f0659bd1c5abaa54092e22b8cc0
Toby_ <tobybarrett1993@gmail.com>
2021-11-09 01:38:31 +0000

    min max variable values, key up function. if datatype selected twice the key up function don't work

--- website/static/index.js
+++ website/static/index.js
@@ -15,12 +15,12 @@ $(document).on('click', '.addfield', function () {
         '                <li><a class="dropdown-item" value=1  >Char          </a></li>\n' +
         '                <li><a class="dropdown-item" value=2  >Varchar       </a></li>\n' +
         '                <li><a class="dropdown-item" value=3  >Nchar         </a></li>\n' +
-        '                <li><a class="dropdown-item" value=4  >NvarChar      </a></li>\n' +
+        '                <li><a class="dropdown-item" value=4  >Nvarchar      </a></li>\n' +
         '                <li><a class="dropdown-item" value=5  >Bit           </a></li>\n' +
         '                <li><a class="dropdown-item" value=6  >Tinybit       </a></li>\n' +
         '                <li><a class="dropdown-item" value=7  >Smallint      </a></li>\n' +
         '                <li><a class="dropdown-item" value=8  >Int           </a></li>\n' +
-        '                <li><a class="dropdown-item" value=9 >Bigint        </a></li>\n' +
+        '                <li><a class="dropdown-item" value=9  >Bigint        </a></li>\n' +
         '                <li><a class="dropdown-item" value=10 >Decimal       </a></li>\n' +
         '                <li><a class="dropdown-item" value=11 >Numeric       </a></li>\n' +
         '                <li><a class="dropdown-item" value=12 >Smallmoney    </a></li>\n' +
@@ -39,20 +39,37 @@ $(document).on('click', '.addfield', function () {
         '            </div>\n' +
         '          </div>\n' +
         '          <div class="col-2" >\n' +
-        '            <input type="number" class="form-control variable" name="variables" placeholder="n/a" value="">\n' +
+        '            <input type="number" class="form-control variable" disabled name="variables" placeholder="n/a" value="" >\n' +
         '          </div>\n' +
-        '          <div class="col-2" >100</div>\n' +
-        '          <div class="col-2" >1000</div>\n';
+        '          <div class="col-2 MinBytes" >0</div>\n' +
+        '          <div class="col-2 MaxBytes" >0</div>\n';
     document.getElementById('fields'+T).appendChild(div);
+
 });
 
 $(document).on('click', '.dropdown-menu li a', function () {
+    //update dropdown
     $(this).parents(".dropdown").find('.btn').html($(this).text());
+    //update variable
+    $(this).parents(".row").find('.variable').val('');
     var sel = $(this).attr("value");
     var variable = ['n/a','n','n','n','n','n/a','n/a','n/a','n/a','n/a','p','p','n/a','n/a','n/a','n','n/a','n/a','p','p','n/a','n/a','n','n'];
     $(this).parents(".row").find('.variable').attr('placeholder',variable[sel]);
-    $(this).parents(".row").find('.variable').val('');
-
+    var hide = [1,0,0,0,0,1,1,1,1,1,0,0,1,1,1,0,1,1,0,0,1,1,0,0];
+    $(this).parents(".row").find('.variable').prop('disabled',hide[sel]);
+    //variable min max values
+    var minvar = [0,1   ,1   ,1   ,1   ,0,0,0,0,0,1 ,1 ,0,0,0,1 ,0,0,0,0,0,0,1   ,1   ]
+    var maxvar = [0,8000,8000,4000,4000,0,0,0,0,0,38,38,0,0,0,53,0,0,7,7,0,0,8000,8000]
+    $(this).parents(".row").find('.variable').attr('min',minvar[sel]);
+    $(this).parents(".row").find('.variable').attr('max',maxvar[sel]);
+    //validate input
+    $(".variable").keyup(function () {
+        var currentvalue = $(this).val()
+        if (currentvalue<minvar[sel])
+            {$(this).val(minvar[sel]);}
+        else if (currentvalue>maxvar[sel])
+            {$(this).val(maxvar[sel]);}
+    });
 
 });
 
@@ -68,14 +85,14 @@ $(function()
         '      <div class="row">\n' +
         '        <div class="textonly col-2"  align="right">Input Table Name:</div>\n' +
         '        <div class="col-2" >\n' +
-        '          <input type="text" class="form-control" name="Table" placeholder="Table Name"/>\n' +
+        '          <input type="text" class="form-control tname" id="tname" name="Table" placeholder="Table Name"/>\n' +
         '        </div>\n' +
         '      </div>\n' +
         '\n' +
         '      <div class="row">\n' +
-        '        <div class="textonly col-2" align="right" >Records Expected:</div>\n' +
+        '        <div class="textonly col-2" align="right" >Records:</div>\n' +
         '        <div class="col-2" >\n' +
-        '          <input type="text" class="form-control" id="Records" name="Records" placeholder="Records"/>\n' +
+        '          <input type="number" class="form-control records" id="Records" name="Records" placeholder="Records" min=1 />\n' +
         '        </div>\n' +
         '      </div>\n' +
         '      <div class="row">\n' +
@@ -86,6 +103,13 @@ $(function()
         '        <div class="textonly col-2" >Maximum Bytes</div>\n' +
         '      </div>\n' +
         '      <div id="fields'+T+'">\n' +
+        '      </div>\n' +
+        '      <div class="row">\n' +
+        '        <div class="textonly col-2"  ></div>\n' +
+        '        <div class="textonly col-2"  ></div>\n' +
+        '        <div class="textonly col-2" align="right"  >Total per record:</div>\n' +
+        '        <div class="textonly col-2" value=0 >0</div>\n' +
+        '        <div class="textonly col-2" value=0 >0</div>\n' +
         '      </div>\n' +
         '      <div class="row">\n' +
         '        <div class="textonly col-2"  ></div>\n' +
@@ -104,6 +128,7 @@ $(function()
 
     document.getElementById('wrapper').appendChild(div);
     T++;
+
     });
 });
 
